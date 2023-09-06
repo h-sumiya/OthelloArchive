dd07a906f4e0d3328364ce846aec0a2e6d261d29
pokopokowahaa <141805673+pokopokowahaa@users.noreply.github.com>
2023-09-06 18:34:50 +0900

    please do not mind

--- ai-othello/src/logic.js
+++ ai-othello/src/logic.js
@@ -7,3 +7,10 @@ export function legal_moves(data, color) {
 export function flip(data, color, position) {
     return;// new data
 }
+
+//  date display
+var     self.dires = [-10, -9, -8, -1, 1, 8, 9, 10]
+        self.discs = ' - o x\n'
+        self.bamed = [0 if i % 9 else 3 for i in range(91)]
+        self.bamed[40] = self.bamed[50] = self.turn = 1
+        self.bamed[41] = self.bamed[49] = 2
