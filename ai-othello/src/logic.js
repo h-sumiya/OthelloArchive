3b62d7d438c7dc80eaf9655d53b0db68c5e85f7e
pokopokowahaa <141805673+pokopokowahaa@users.noreply.github.com>
2023-09-08 16:01:09 +0900

    finish

--- ai-othello/src/logic.js
+++ ai-othello/src/logic.js
@@ -1,15 +1,94 @@
-// data [int;64] Null: 0 Black: 1 White: 2
 
-export function legal_moves(data, color) {
-    return;// [move]
+const moves = [
+    [1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]
+];
+
+function inrange(x, y) {
+    if (!(0 <= x && x < 8)) {
+        return false
+    } else if (!(0 <= y && y < 8)) {
+        return false
+    } else {
+        return true
+    }
+}
+export function filp(date, pos, color) {
+    let filps = [];
+    let oppcolor = 3 - color;
+    let x = pos % 8;
+    let y = Math.floor(pos / 8);
+    if (date[pos] !== 0) {
+        throw 'すでに石が置かれていました';
+    }
+    for (let i = 0; i < 8; i++) {
+        let move_x = moves[i][0];
+        let move_y = moves[i][1];
+        let tmpfilps = [];
+        let next_x = x + move_x;
+        let next_y = y + move_y;
+        while (inrange(next_x, next_y) && date[next_y * 8 + next_x] == oppcolor) {
+            tmpfilps.push(next_y * 8 + next_x);
+            next_x += move_x;
+            next_y += move_y;
+
+        } if (inrange(next_x, next_y) && date[next_y * 8 + next_x] == color) {
+            filps = [...filps, ...tmpfilps]
+        }
+
+    }
+    if (filps.length == 0) {
+        throw '置けません';
+    }
+    let res = [...date];
+    for (let i = 0; i < filps.length; i++) {
+        res[filps[i]] = color
+    }
+    res[pos] = color;
+    return res;
 }
 
-export function flip(data, color, position) {
-    return;// new data
+export function check(date, pos, color) {
+    let oppcolor = 3 - color;
+    let x = pos % 8;
+    let y = Math.floor(pos / 8);
+    if (date[pos] !== 0) {
+        return false;
+    }
+    for (let i = 0; i < 8; i++) {
+        let move_x = moves[i][0];
+        let move_y = moves[i][1];
+        let next_x = x + move_x;
+        let next_y = y + move_y;
+        let flag = false;
+        while (inrange(next_x, next_y) && date[next_y * 8 + next_x] == oppcolor) {
+            flag = true;
+            next_x += move_x;
+            next_y += move_y;
+
+        } if (inrange(next_x, next_y) && date[next_y * 8 + next_x] == color && flag) {
+            return true;
+        }
+
+    }
+    return false
 }
 
-//  date display
-class OthelloPractice {
-    var dires = [-10, -9, -8, -1, 1, 8, 9, 10], 
-    discs = ' 0 1 2\n', 
-    bamed = [
+export function legal_moves(date, color) {
+    let moves = [];
+    for (let i = 0; i < 64; i++) {
+        if (check(date, i, color)) {
+            moves.push(i);
+        }
+    }
+    return moves
+}
+
+export function have_legal_moves(date, color) {
+    for (let i = 0; i < 64; i++) {
+        if (check(date, i, color)) {
+            return true;
+        }
+    }
+    return false;
+
+}
